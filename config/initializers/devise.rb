6a7b7e031fac96a1da2f0800aeefb907eb336135
Igor Juraszek <igorjuraszek@gmail.com>
2024-01-30 09:04:29 +0100

    somme changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -306,8 +306,8 @@ Devise.setup do |config|
   config.responder.redirect_status = :see_other
 
   config.jwt do |jwt|
-    # jwt.secret = ENV.fetch('SECRET_KEY_BASE')
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.secret = ENV.fetch('SECRET_KEY_BASE')
+    # jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
       ['POST', %r{^/sign_in$}]
     ]
